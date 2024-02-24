1a8795e32b9fc35c00d9cbf8455773bfd348a222
Sridhar-Konidela <sridharkonidela@outlook.com>
2024-02-24 10:53:58 +0530

    Add body section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
 import HomeHeader from '@/components/HomeHeader'
 import React from 'react'
+import Image from 'next/image'
+import HomeSearch from '@/components/HomeSearch'
 
 const Home = () => {
   return (
     <>
       <HomeHeader/>
+      <div className='flex flex-col items-center mt-24'>
+        <Image  src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/800px-Google_2015_logo.svg.png' alt='google image' width={300} height={300} priority style={{width:'auto'}}/>
+        <HomeSearch/>
+      </div>
     </>
   )
 }
