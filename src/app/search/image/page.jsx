884713f293fa1ff7a82b7d3d12d072a71800c0e0
Sridhar-Konidela <sridharkonidela@outlook.com>
2024-02-25 16:34:59 +0530

    Hide API key

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -6,9 +6,7 @@ export default async function ImageSearchPage({searchParams}) {
     await new Promise((resolve)=>setTimeout(resolve,1500));
     const response=await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}
     &cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${startIndex}`);
-    // const response=await fetch(
-    //     `https://www.googleapis.com/customsearch/v1?key=AIzaSyAbWZJcQgOHDi5TgRkrk26nqNm-MgBZiQQ&cx=878a73e23f718457f&q=${searchParams.searchTerm}`
-    // )
+    
     if(!response.ok) throw new Error("Something went wrong");
     const data=await response.json();
     const results=data.items;
